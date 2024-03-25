38cfde1d376d3e05f3b8f8d67a3789cb1db21699
kahchun.wong <kahchun.wong@softspace.com.my>
2024-03-25 12:07:56 +0800

    updated Container component

--- components/content/container/Container.vue
+++ components/content/container/Container.vue
@@ -1,15 +1,21 @@
 <template>
     <div :class="`ss-container ss-container-${type} p-component`">
         <div class="ss-container-wrapper">
-            <p class="ss-container-header">
-                <slot name="container-icon" class="ss-container-icon">
-                    <i :class="icon ? icon : defaultIcon"></i>
-                </slot>
-                <slot name="container-title" class="ss-container-title">
-                    <span>{{ title ? title : defaultTitle }}</span>
-                </slot>
-            </p>
-            <slot class="ss-container-content"></slot>
+            <div class="ss-container-header">
+                <span class="ss-container-icon">
+                    <slot name="container-icon" >
+                        <i :class="icon ? icon : defaultIcon"></i>
+                    </slot>
+                </span>
+                <span class="ss-container-title">
+                    <slot name="container-title" >
+                        {{ title ? title : defaultTitle }}
+                    </slot>
+                </span>
+            </div>
+            <div class="ss-container-content">
+                <slot></slot>
+            </div>
         </div>
     </div>
 </template>
