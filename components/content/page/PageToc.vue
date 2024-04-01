d4ed6c4022b0a723c1a43e29371eb0994421760e
kahchun.wong <kahchun.wong@softspace.com.my>
2024-04-01 10:22:05 +0800

    updated components

--- components/content/page/PageToc.vue
+++ components/content/page/PageToc.vue
@@ -1,11 +1,11 @@
 <template>
-    <nav class="ss-toc">
+    <nav v-if="toc && toc.links?.length" class="ss-toc">
         <div class="ss-toc-wrapper">
             <Button class="ss-toc-header" @click="toggleToc">
                 <span class="ss-toc-header-title">On this page</span>
                 <ExpandToggleIcon :expanded="!hidden" />
             </Button>
-            <PageTocLinks v-if="toc && toc.links" :links="toc.links" :class="{ 'hidden': hidden }" />
+            <PageTocLinks :links="toc.links" :class="{ 'hidden': hidden }" />
         </div>
     </nav>
 </template>
