b228ee1c05de16c17fd80b50664915315531e4af
kahchun.wong <kahchun.wong@softspace.com.my>
2024-04-18 13:32:41 +0800

    update

--- pages/reference/[...slug].vue
+++ pages/reference/[...slug].vue
@@ -1,11 +1,15 @@
 <template>
-  <ApiPage v-slot="{ doc }">
-    <PageHeader/>
-    <PageBody>
-        <ContentRenderer :value="doc" />
-    </PageBody>
-    <PageSurroundNav />
-  </ApiPage>
+  <main class="ss-page ss-api-page">
+        <ContentDoc v-slot="{ doc }">
+            <article>
+              <PageHeader/>
+              <PageBody>
+                  <ContentRenderer :value="doc" />
+              </PageBody>
+              <PageSurroundNav />
+            </article>
+        </ContentDoc>
+    </main>
 </template>
 
 <script setup lang="ts">
